f3597f55bb3ddee7ca74ef689862302bf4af8fb3
Karl Xu <karl.yogi.xu@gmail.com>
2019-03-27 23:07:30 +0800

    add environment variable nodes from keyvalut to serviceManifest

--- add-environment-variable-to-ServiceManifest/save_to_file.groovy
+++ add-environment-variable-to-ServiceManifest/save_to_file.groovy
@@ -1,42 +1,33 @@
 import groovy.xml.QName
 import groovy.json.JsonSlurper
 
-// // get key vault string
-// def key_vault_secret_id = "https://testkx.vault.azure.cn/secrets/test-getting-environment"
-// def get_key_vault_secret = """
-//   az keyvault secret show --id ${key_vault_secret_id} | jq -r '.value'
-// """
+// get key vault string
+def key_vault_secret_id = "https://testkx.vault.azure.cn/secrets/test-getting-environment"
+def get_key_vault_secret = """
+  az keyvault secret show --id ${key_vault_secret_id} | jq -r '.value'
+"""
 
-// def string = ["bash", "-c", get_key_vault_secret].execute().text
+def string = ["bash", "-c", get_key_vault_secret].execute().text
 
 
 
-// // Loop json
-// def jsonSlurper = new JsonSlurper()
-// assert jsonSlurper.parseText(string) instanceof ArrayList
-
-// for (Map obj : jsonSlurper.parseText(string)) {
-//   def command = """
-//     <EnvironmentVariable Name=\"${obj.key}\" Value=\"${obj.value}\"/>
-//   """
-//   println command
-// }
-
+// Loop json
+def jsonSlurper = new JsonSlurper()
+assert jsonSlurper.parseText(string) instanceof ArrayList
 
 def parser = new XmlParser()
-
-// read from file 
 def ServiceManifest = new XmlParser().parse(new File('ServiceManifest.xml'))
 
-def EnvironmentVariable = new Node(
-  null, 
-  new QName("EnvironmentVariable"), 
-  [Name: 'DATABASE', Value: "mysql"]
-)
+for (Map obj : jsonSlurper.parseText(string)) {
+  def EnvironmentVariable = new Node(
+    null, 
+    new QName("EnvironmentVariable"), 
+    [Name: "${obj.key}", Value: "${obj.value}"]
+  )
+  
+  ServiceManifest.CodePackage.EnvironmentVariables[0].children().add(0, EnvironmentVariable)
+}
 
-ServiceManifest.CodePackage.EnvironmentVariables[0].children().add(0, EnvironmentVariable)
-
-println ServiceManifest.CodePackage.EnvironmentVariables.EnvironmentVariable.text()
 
 FileWriter fileWriter = new FileWriter('save_file.xml')
 XmlNodePrinter nodePrinter = new XmlNodePrinter(new PrintWriter(fileWriter))
