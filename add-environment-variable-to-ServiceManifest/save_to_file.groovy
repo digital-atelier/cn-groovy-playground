bc2634970ac3ad269b25ce7394ef0de533acb79f
Karl Xu <karl.yogi.xu@gmail.com>
2019-03-27 23:03:34 +0800

    add EnvironmentVariable node to servicemanifest

--- add-environment-variable-to-ServiceManifest/save_to_file.groovy
+++ add-environment-variable-to-ServiceManifest/save_to_file.groovy
@@ -1,16 +1,38 @@
 import groovy.xml.QName
+import groovy.json.JsonSlurper
+
+// // get key vault string
+// def key_vault_secret_id = "https://testkx.vault.azure.cn/secrets/test-getting-environment"
+// def get_key_vault_secret = """
+//   az keyvault secret show --id ${key_vault_secret_id} | jq -r '.value'
+// """
+
+// def string = ["bash", "-c", get_key_vault_secret].execute().text
+
+
+
+// // Loop json
+// def jsonSlurper = new JsonSlurper()
+// assert jsonSlurper.parseText(string) instanceof ArrayList
+
+// for (Map obj : jsonSlurper.parseText(string)) {
+//   def command = """
+//     <EnvironmentVariable Name=\"${obj.key}\" Value=\"${obj.value}\"/>
+//   """
+//   println command
+// }
+
 
 def parser = new XmlParser()
 
 // read from file 
 def ServiceManifest = new XmlParser().parse(new File('ServiceManifest.xml'))
 
-def EnvironmentVariable = parser.createNode(
-    null,
-    new QName("EnvironmentVariable"),
-    [:]
+def EnvironmentVariable = new Node(
+  null, 
+  new QName("EnvironmentVariable"), 
+  [Name: 'DATABASE', Value: "mysql"]
 )
-EnvironmentVariable.value = 'test'
 
 ServiceManifest.CodePackage.EnvironmentVariables[0].children().add(0, EnvironmentVariable)
 
