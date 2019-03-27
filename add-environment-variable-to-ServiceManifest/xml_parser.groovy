98743371654486eeb0abe8a3a192bf5c901368c0
Karl Xu <karl.yogi.xu@gmail.com>
2019-03-27 16:30:38 +0800

    parse environment variable

--- add-environment-variable-to-ServiceManifest/xml_parser.groovy
+++ add-environment-variable-to-ServiceManifest/xml_parser.groovy
@@ -1,5 +1,13 @@
-def test = new XmlSlurper().parse(new File('ServiceManifest.xml')) 
+def snippet = '''
+<EnvironmentVariables>
+    <EnvironmentVariable Name="MyEnvVariable" Value="DefaultValue"/>
+    <EnvironmentVariable Name="HttpGatewayPort" Value="19080"/>
+</EnvironmentVariables>
+'''
+
+
+def test = new XmlSlurper().parseText(snippet) 
 
 assert test instanceof groovy.util.slurpersupport.GPathResult 
 
-println test.CodePackage.EntryPoint.ContainerHost.ImageName
+println test.EnvironmentVariable[0]['@Name']
